2da3494790a5d251244f17004f919848eb574daf
Ksenia-Surface <kvitkobioinf@gmail.com>
2020-04-22 04:24:50 +0200

    automatyzacja trimmomatic

--- zad 4/auto_fastqc.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-pliki=*.fastq
-for p in $pliki
-do
-  fastqc -o analiza_jakosci/ $p
-done
-
-echo
-echo "Analiza jakosci zakonczona"
---
+++ zad_3/auto_trimmomatic.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+while getopts i:o:e:a:t:h option; do
+  case $option in
+    i) wejscie=$OPTARG;;
+    o) wyjscie=$OPTARG;;
+    e) ext=$OPTARG;;
+    a) adapter=$OPTARG;;
+    t) trimmomatic=$OPTARG;;
+    h) echo "-i folder wejscia, -o folder wyjscia, -e rozszerzenie pliku, -a plik adaptera, -t lokalizacja pliku java trimmonatic (do znalezienia przez which trimmomatic)"
+       echo "FOLDERY BEZ! KONCZACEGO znaku /"
+       exit;;
+  esac
+done
+
+
+iloscPlikow="$(ls $wejscie/*.$ext -l | wc -l)"
+echo "Znaleziono plikow: $iloscPlikow"
+
+SRA_wejsciowe=""
+SRA_wyjsciowe=""
+
+pliki=$wejscie/*.$ext
+for p in $pliki
+do
+    plik=$(basename -- "${wejcie}${p}")
+    SRA_wejsciowe="$SRA_wejsciowe ${wejscie}/${plik%.*}.$ext"
+    SRA_wyjsciowe="$SRA_wyjsciowe ${wyjscie}/${plik%.*}_trimmed.fq"
+    SRA_wyjsciowe="$SRA_wyjsciowe ${wyjscie}/${plik%.*}_unpaired.fq"
+done
+
+echo
+java -jar $trimmomatic PE $SRA_wejsciowe $SRA_wyjsciowe ILLUMINACLIP:$wejscie/$adapter:2:30:5
