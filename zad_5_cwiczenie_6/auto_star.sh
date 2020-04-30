e77e022ea270a6f16ffd26248ef0eb52c3c2a985
Ksenia-Surface <kvitkobioinf@gmail.com>
2020-04-30 23:29:05 +0200

    poczatek zad 5 (cw 6)

---
+++ zad_5_cwiczenie_6/auto_star.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+while getopts i:o:e:a:t:h option; do
+  case $option in
+    i) wejscie=$OPTARG;;
+    o) wyjscie=$OPTARG;;
+    e) ext=$OPTARG;;
+    h) echo "-i folder wejscia, -o folder wyjscia, -e rozszerzenie pliku"
+       echo "FOLDERY BEZ! KONCZACEGO znaku /"
+       exit;;
+  esac
+done
+
+
+iloscPlikow="$(ls $wejscie/*.$ext -l | wc -l)"
+echo "Znaleziono plikow: $iloscPlikow"
+
+STAR_wejsciowe=""
+
+pliki=$wejscie/*.$ext
+i=0
+for p in $pliki
+do
+    plik=$(basename -- "${wejcie}${p}")
+    STAR_wejsciowe="$STAR_wejsciowe ${wejscie}/${plik%.*}.$ext"
+    ((i=i+1))
+    if ! (($i % 2)); then
+      echo
+      echo "Pracuje na $STAR_wejsciowe"
+
+      # STAR --runThreadN 2 --runMode genomeGenerate --genomeDir $wyjscie --genomeFastaFiles $STAR_wejsciowe
+      /bioapp/STAR-2.7.3a/source/STAR --runThreadN 2 --runMode genomeGenerate --genomeDir $wyjscie --genomeFastaFiles $STAR_wejsciowe
+
+      STAR_wejsciowe=""
+    fi
+done
