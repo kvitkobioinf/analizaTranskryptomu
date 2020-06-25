3dfb3b913b055e5de208e6cc45521754e982d4b8
Ksenia Kvitko (serwer Lupin) <s119494@lupin.upwr.edu.pl>
2020-06-26 01:57:35 +0200

    Konwersja SRA->Fastq

--- projekt/1_pobieranie.sh
+++ projekt/1_pobieranie.sh
@@ -9,3 +9,19 @@ sed -i 's/\t/\n/g' SRR_Acc_List.txt
 
 # usuwanie folderu ncbi w katalogu domowym (domyślna lokalizacja pobierania plików SRA przez program prefetch) jeśli wcześniej istniał (jeśli nie istniał - ignorowanie informacji o błędzie)
 rm -r ~/ncbi 2> /dev/null
+
+# pobranie plików SRA z listy w pliku tekstowym
+prefetch $(<SRR_Acc_List.txt)
+
+# utworzenie folderu dla pobranych plików
+mkdir PRJNA313294_SRA
+# przeniesienie ich do folderu docelowego
+mv ~/ncbi/public/sra/*.sra PRJNA313294_SRA/
+# usunięcie plików z domyślnego katalogu pobrania (j.w.)
+rm -r ~/ncbi 2> /dev/null
+
+# konwersja plików SRA na odpowiednie pliki fastq (metoda szybsza niż pobranie plików formatu fastq bezpośrednio przez fastq-dump
+for file in PRJNA313294_SRA/*.sra
+do
+	fasterq-dump ${file}
+done
