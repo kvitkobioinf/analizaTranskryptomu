836e64b2f32b6baf45221d7c925c76780c3645c3
Ksenia Kvitko (serwer Lupin) <s119494@lupin.upwr.edu.pl>
2020-06-26 20:43:10 +0200

    Generowanie i pobieranie raportów QC

--- projekt/4_prepareQualityChecksForDownload.sh
+++ projekt/4_prepareQualityChecksForDownload.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 # przygotowanie plików html z raportami jakości do pobrania z serwera celem oceny wizualnej
-zip qualityCheck.zip ./qualityChecks/*.html
-mv ./qualityChecks/qualityCheck.zip ~/
+zip qualityCheck.zip ./qualityChecks/*.html >/dev/null
+mv qualityCheck.zip ~/
 
 echo 'Komenda do pobrania raportów'
 echo 'scp s119494@lupin.upwr.edu.pl:~/qualityCheck.zip ./qualityCheck.zip'
+echo ''
+echo 'Po pobraniu usuń plik komendą'
+echo 'rm ~/qualityCheck.zip'
