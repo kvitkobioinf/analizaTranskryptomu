8bd412e26360ef692a4e9893abd4f186bfc88326
Ksenia Kvitko (serwer Lupin) <s119494@lupin.upwr.edu.pl>
2020-06-27 18:59:19 +0200

    Zliczanie odczytów osobno dla paired i single-end oraz wspólnie. Poprawiona nazwa kolumny z gene_name (na plikach z zajęć) na gene_id (plik hg19.gtf)

--- projekt/10_featureCounts.sh
+++ projekt/10_featureCounts.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-/bioapp/subread-2.0.0-source/bin/featureCounts -a ./hg19.gtf -g gene_name -o ./counts.txt ./STAR_mapped/*/*_sorted.bam
+mkdir COUNTS 2>/dev/null
+
+/bioapp/subread-2.0.0-source/bin/featureCounts -a ./hg19.gtf -g gene_id -o ./COUNTS/counts_ALL.txt ./STAR_mapped/*/*_sorted.bam
+
+/bioapp/subread-2.0.0-source/bin/featureCounts -a ./hg19.gtf -g gene_id -o ./COUNTS/counts_SE.txt ./STAR_mapped/SRR31944*/*_sorted.bam
+
+/bioapp/subread-2.0.0-source/bin/featureCounts -a ./hg19.gtf -g gene_id -p -o ./COUNTS/counts_PE.txt ./STAR_mapped/SRR319154*/*_sorted.bam
