ff5838db052ea453ea3f690998a6b3488bc5bc59
Ksenia Kvitko (serwer Lupin) <s119494@lupin.upwr.edu.pl>
2020-06-28 01:49:09 +0200

    PCA, heatmapa. TODO analiza DE względem infekcji wirusem Zika, nie urządzenia.

--- projekt/11_normalization_DE_PCA_heatmap.R
+++ projekt/11_normalization_DE_PCA_heatmap.R
@@ -13,7 +13,15 @@ print(SRAdata)
 counts <- read.delim("~/projekt/analizaTranskryptomu/projekt/hg19/COUNTS/counts_ALL.txt", comment.char="#")
 colnames(counts)[7:14] <- SRAshortcuts
 
-normalize <- function(data, dds_only) {
+normalize <- function(data) {
+  log_data <- rlog(data)
+  normalized_data<- assay(log_data)
+  normalized_data <- as.data.frame(normalized_data)
+  
+  return(normalized_data)
+}
+
+dds <- function(data) {
   countData <- data[,7:14]
   rownames(countData) = data$Geneid
   samples <- names(countData)
@@ -22,19 +30,11 @@ normalize <- function(data, dds_only) {
   condition <- factor(c(cond_1, cond_2))
   colData <- data.frame(samples = samples, condition = condition)
   dds <- DESeqDataSetFromMatrix(countData = countData, colData = colData, design = ~condition)
-  if(dds_only)
-    return(dds)
   
-  dss1 <- estimateSizeFactors(dds)
-  
-  log_data <- rlog(dds)
-  normalized_data<- assay(log_data)
-  normalized_data <- as.data.frame(normalized_data)
-  
-  return(normalized_data)
+  return(dds)
 }
 
-analyseDESeq = function(data) {
+analyseDE = function(data) {
   dds <- DESeq(data)
   res <- results(dds)
   
@@ -49,52 +49,7 @@ analyseDESeq = function(data) {
   return(dds)
 }
 
-desCounts <- analyseDESeq(normalize(counts, T))
-
-drawHeatmap <- function(data){
-  threshold <- 10
-  data <- data[
-      data$M1_MiSeq > threshold | 
-      data$M2_MiSeq > threshold | 
-      data$Z1_MiSeq > threshold | 
-      data$Z2_MiSeq > threshold | 
-      data$M1_NextSeq > threshold | 
-      data$M2_NextSeq > threshold |
-      data$Z1_NextSeq > threshold |
-      data$Z2_NextSeq > threshold
-    , 
-    ]
-  data <- data.frame(data$M1_MiSeq, data$M2_MiSeq, data$M1_NextSeq, data$M2_NextSeq, data$Z1_NextSeq, data$Z2_NextSeq, data$Z1_MiSeq, data$Z2_MiSeq)
-  #renamed_data <- data
-  #colnames(renamed_data) = c("HBR1","HBR2","HBR3", "UHR1", "UHR2", "UHR3")
-  names(data) = c("M28", "M29", "Z30", "Z31", "M42", "M43", "Z44", "Z45")
-  
-  Heatmap(data , cluster_columns = FALSE,
-          row_names_side = "left",
-          row_dend_sid = "left",
-          row_names_gp=gpar(cex = 0.8))
-}
-
-my_heat = function(dane){
-  expression = 9
-  dane = dane[dane$mock28.bam > expression | dane$mock29.bam> expression | dane$zika30.bam > expression | dane$zika31.bam > expression | dane$mock42.bam > expression | dane$mock43.bam > expression| dane$zika44.bam > expression |dane$zika45.bam > expression, ]
-  print(dane)
-  dane1 = data.frame(dane$mock28.bam, dane$mock29.bam, dane$zika30.bam, dane$zika31.bam, dane$mock42.bam, dane$mock43.bam, dane$zika44.bam, dane$zika45.bam)
-  dane1 = data.frame(dane$mock28.bam, dane$mock29.bam, dane$mock42.bam, dane$mock43.bam, dane$zika44.bam, dane$zika45.bam, dane$zika30.bam, dane$zika31.bam)
-  names(dane1) = c("M28", "M29", "Z30", "Z31", "M42", "M43", "Z44", "Z45")
-  
-  Heatmap(dane1, cluster_columns=FALSE,
-          row_names_side = "left",
-          row_dend_sid = "left",
-          row_names_gp=gpar(cex=0.5))
-}
-nor = normal(counts)
-
-my_heat(nor)
-drawHeatmap(normalizedCounts)
-
-
-
+analyseDE(dds(counts))
 
 PCAnalysis <- function(data) {
   gene_data <- data[7:14]
@@ -117,4 +72,28 @@ PCAnalysis <- function(data) {
     ggtitle("PCA Plot")
   
 }
+
 PCAnalysis(counts)
+
+drawHeatmap <- function(data){
+  threshold <- 10
+  data <- data[
+      data$M1_MiSeq > threshold | 
+      data$M2_MiSeq > threshold | 
+      data$Z1_MiSeq > threshold | 
+      data$Z2_MiSeq > threshold | 
+      data$M1_NextSeq > threshold | 
+      data$M2_NextSeq > threshold |
+      data$Z1_NextSeq > threshold |
+      data$Z2_NextSeq > threshold
+    , ]
+  data <- data.frame(data$M1_MiSeq, data$M2_MiSeq, data$M1_NextSeq, data$M2_NextSeq, data$Z1_NextSeq, data$Z2_NextSeq, data$Z1_MiSeq, data$Z2_MiSeq)
+  names(data) = c(SRAshortcuts[1], SRAshortcuts[2], SRAshortcuts[5], SRAshortcuts[6], SRAshortcuts[3], SRAshortcuts[4], SRAshortcuts[7], SRAshortcuts[8])
+  
+  Heatmap(data , cluster_columns = FALSE,
+          row_names_side = "left",
+          row_dend_sid = "left",
+          row_names_gp=gpar(cex = 0.8))
+}
+
+drawHeatmap(normalize(dds(counts)))
