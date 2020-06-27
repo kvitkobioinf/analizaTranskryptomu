608bba7d5aaa57c16ec8128333b1320b1e8079a3
Ksenia Kvitko (serwer Lupin) <s119494@lupin.upwr.edu.pl>
2020-06-27 20:17:40 +0200

    Uaktualniony adres genomu referencyjnego - zgodnie z zakładką NCBI otwartą podczas wideowykładu

--- projekt/6_referenceChromosome.sh
+++ projekt/6_referenceChromosome.sh
@@ -2,6 +2,4 @@
 
 # https://ftp.ncbi.nlm.nih.gov/genomes/archive/old_genbank/Eukaryotes/vertebrates_mammals/Homo_sapiens/GRCh37/Primary_Assembly/assembled_chromosomes/FASTA/
 
-wget https://ftp.ncbi.nlm.nih.gov/genomes/archive/old_genbank/Eukaryotes/vertebrates_mammals/Homo_sapiens/GRCh37/Primary_Assembly/assembled_chromosomes/FASTA/chr19.fa.gz
-
-wget https://ftp.ncbi.nlm.nih.gov/genomes/archive/old_genbank/Eukaryotes/vertebrates_mammals/Homo_sapiens/GRCh37/Primary_Assembly/assembled_chromosomes/FASTA/chr19.rm.out.gz
+#https://www.ncbi.nlm.nih.gov/assembly/GCF_000001405.13/
