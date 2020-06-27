f7277b4607f3a29c330a26add56804212adfca9f
Ksenia Kvitko (serwer Lupin) <s119494@lupin.upwr.edu.pl>
2020-06-27 22:23:08 +0200

    Źródła plików FASTA (.fna) i opisów lokalizacji genów (.gtf) genomu

--- projekt/6_referenceChromosome.sh
+++ projekt/6_referenceChromosome.sh
@@ -3,3 +3,7 @@
 # https://ftp.ncbi.nlm.nih.gov/genomes/archive/old_genbank/Eukaryotes/vertebrates_mammals/Homo_sapiens/GRCh37/Primary_Assembly/assembled_chromosomes/FASTA/
 
 #https://www.ncbi.nlm.nih.gov/assembly/GCF_000001405.13/
+
+# nowsza wersja - https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/001/405/GCF_000001405.25_GRCh37.p13/
+# wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/001/405/GCF_000001405.39_GRCh38.p13/GCF_000001405.39_GRCh38.p13_genomic.fna.gz
+# wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/001/405/GCF_000001405.39_GRCh38.p13/GCF_000001405.39_GRCh38.p13_genomic.gtf.gz
